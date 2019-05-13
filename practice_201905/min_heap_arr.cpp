bbc3c3d111a8dcb6f64de06745d169eecb70eb02
bruce <bruce.kim.it@gmail.com>
2019-05-13 23:35:46 +0900

    2019.05.13 min heap practice

--- practice_201905/min_heap_arr.cpp
+++ practice_201905/min_heap_arr.cpp
@@ -6,47 +6,44 @@ class xminheap {
         xminheap(int _capa) : capa(_capa), sz(0) {
             arr = new int[capa]{0};
         }
-        virtual ~xminheap() {
-            delete []arr;
-        }
+        virtual ~xminheap() { delete []arr; }
         void swap(int &a, int &b) {
             int tmp = a;
             a = b;
             b = tmp;
         }
-        int parent(int v) {
-            return arr[(v-1)/2];
-        }
-        int left(int v) {
-            return arr[v*2+1];
-        }
-        int right(int v) {
-            return arr[v*2+2];
-        }
-        int getMin() {
-            return arr[0];
-        }
+        int parent(int v) { return (v-1)/2; }
+        int left(int v) { return (v*2)+1; }
+        int right(int v) { return (v*2)+2; }
+        int getMin() { return arr[0]; }
         int extractMin() {
             if (sz <= 0) {
                 return int_min;
             } else if (sz == 1) {
-                return arr[--sz];
+                --sz;
+                return arr[0];
             } else {
+                int root = arr[0];
+                arr[0] = arr[sz-1];
+                --sz;
+                heapify(0);
+                return 0;
             }
         }
         /* delete value at index i */
         void deleteKey(int i) {
-            decreasekey(i, int_min);
+            decreaseKey(i, int_min);
             extractMin();
         }
         /* insert value k */
         void insertKey(int k) {
-            if (sz >= capa) {
+            if (sz == capa) {
                 return;
             }
-            int i = sz++;
+            int i = sz;
+            ++sz;
             arr[i] = k;
-            while (i != 0 77 arr[parent(i)] > arr[i]) {
+            while (i != 0 && arr[parent(i)] > arr[i]) {
                 swap(arr[parent(i)], arr[i]);
                 i = parent(i);
             }
@@ -59,6 +56,28 @@ class xminheap {
             }
         }
         void heapify(int v) {
+            int l = left(v);
+            int r = right(v);
+            int smallest = v; /* 2019.05.13
+                                        여기서 min heap인데, 자식 노드 중에서 smallest를 고르는 의미가 있나?
+                                        ans) 자식들보다 부모노드가 작다는 property를 보장하기 위해서는 자식 중에서도 더 작은 놈을 선별해서 비교해야 함
+                                        */
+            if (l < sz && arr[l] < arr[v]) {
+                smallest = l;
+            }
+            if (r < sz && arr[r] < arr[smallest]) {
+                smallest = r;
+            }
+            if (smallest != v) {
+                swap(arr[smallest], arr[v]);
+                heapify(smallest);
+            }
+        }
+        void print() {
+            for (int i=0; i<sz; ++i) {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
         }
 
     private:
@@ -69,6 +88,18 @@ class xminheap {
 
 void solve() {
     xminheap minheap(10);
+    minheap.insertKey(5);
+    minheap.insertKey(4);
+    minheap.insertKey(3);
+    minheap.insertKey(2);
+    minheap.insertKey(1);
+    minheap.print();
+
+    printf("m %d\n", minheap.extractMin());
+    minheap.insertKey(0);
+    minheap.print();
+    minheap.deleteKey(2);
+    minheap.print();
 }
 
 int main() {

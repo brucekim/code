8a248340999dc90bf0440a2cca9ab1eef3f58e38
bruce <bruce.kim.it@gmail.com>
2019-05-17 00:08:08 +0900

    pass :) swe_pretest 1494 mountail trail

--- practice_201905/swe_pretest_1949_mountain_trail.cpp
+++ practice_201905/swe_pretest_1949_mountain_trail.cpp
@@ -1,6 +1,13 @@
-/*
-*/
 #include <cstdio>
+
+//#define DBG
+#define DBG_STDIN
+
+#ifdef DBG
+#define dbg_msg(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+#define dbg_msg(fmt, ...)
+#endif
 
 const static int const_max_depth = 10000;
 const static int const_err_code = 0x3f3f3f3f;
@@ -9,75 +16,158 @@ const static int const_max_n = 8;
 const static int const_max_k = 5;
 
 struct coord {
-    coord(int _x=0, int _y=0) : x(_x), y(_y) {}
-    int x;
-    int y;
+    coord(int _r=0, int _c=0) : r(_r), c(_c) {}
+    int r;
+    int c;
 };
 
 template <typename T>
-class xstack {
+struct node {
+    node(T _data) : data(_data), prev(nullptr), next(nullptr) {}
+    T data;
+    node<T> *prev;
+    node<T> *next;
+};
+
+template <typename T>
+class xlist_iterator {
     public:
-    xstack(int _capa=const_max_depth) : capa(_capa), sz(0) {
-        arr = new coord[capa]();
+    xlist_iterator(node<T> *it) : current(it) {}
+    xlist_iterator& operator++() {
+        current = current->next;
+        return *this;
     }
-    virtual ~xstack() {
-        delete[] arr;
+    T& operator*() {
+        return current->data;
     }
-    void push_back (T &&c) {
-        if (sz < capa) {
-            arr[sz++] = c;
+    bool operator!=(const xlist_iterator<T> &it) const {
+        return current != it.current;
+    }
+    bool operator==(const xlist_iterator<T> &it) const {
+        return current == it.current;
+    }
+    private:
+    node<T> *current;
+
+};
+
+template <typename T>
+class xlist {
+    public:
+    using iterator = xlist_iterator<T>;
+    xlist() : head(nullptr), tail(nullptr), sz(0) {}
+    virtual ~xlist() {
+        while(head != nullptr) {
+            node<T> *tmp = head;
+            head = head->next;
+            delete tmp;
         }
     }
-    void pop() {
-        if (sz > 0) {
+    void push_back(T data) {
+        node<T> *tmp = new node<T>(data);
+        ++sz;
+        if (head == nullptr) {
+            head = tail = tmp;
+        } else {
+            tmp->prev = tail;
+            tail->next = tmp;
+            tail = tail->next;
+        }
+    }
+    void pop_front() {
+        if (head != nullptr) {
+            node<T> *tmp = head;
+            head = head->next;
+            head->prev = nullptr;
+            delete tmp;
             --sz;
         }
     }
-    T &top() {
-        return arr[sz-1];
+    T &&front() {
+        return head->data;
     }
-    bool isEmpty() {
-        return sz == 0;
-
+    iterator begin() {
+        return iterator(head);
     }
-    int getSize() {
-        return sz;
-    }
-    coord& operator[](int idx) {
-        return arr[idx];
+    iterator end() {
+        return iterator(nullptr);
     }
 
     private:
-    int capa;
     int sz;
-    T *arr;
+    node<T> *head;
+    node<T> *tail;
 };
 
 class solver {
     public:
-        solver(int _n, int _k, int (*_arr)[const_max_n], xstack<coord> *_st) :
-            n(_n), k(_k), arr(_arr), st(_st) {}
+        solver(int _n, int _k, int (*_arr)[const_max_n], xlist<coord> *_list_peak) :
+            n(_n), k(_k), arr(_arr), list_peak(_list_peak), max(-1) {}
+
         void print() {
-            printf("map:\n");
+            dbg_msg("map:\n");
             for (int i=0; i<n; ++i) {
                 for (int j=0; j<n; ++j) {
-                    printf("%d ", arr[i][j]);
+                    dbg_msg("%d ", arr[i][j]);
                 }
-                printf("\n");
+                dbg_msg("\n");
             }
-            printf("highest::\n");
-            for(int i=0; i<st->getSize(); ++i) {
-                printf("(%d %d)", (*st)[i].x, (*st)[i].y);
+            dbg_msg("highest::\n");
+            xlist<coord>::iterator end = list_peak->end();
+            for(xlist<coord>::iterator it = list_peak->begin(); it != end; ++it) {
+                dbg_msg("(%d %d)", (*it).c, (*it).r);
             }
-            printf("\n");
+            dbg_msg("\n");
         }
-        void dfs() {
-            for 
+        void solve() {
+            xlist<coord>::iterator end = list_peak->end();
+            for(xlist<coord>::iterator it = list_peak->begin(); it != end; ++it) {
+                int r1 = (*it).r;
+                int c1 = (*it).c;
+                int h1 = arr[c1][r1];
+                dbg_msg("peak..(%d %d) h(%d)\n", c1, r1, h1);
+                visited[c1][r1] = true;
+                dfs(h1, r1, c1, false, 1);
+                visited[c1][r1] = false;
+            }
+        }
+        void dfs(int h1, int r1, int c1, bool kcut, int dist) {
+            dbg_msg("(%d %d) h(%d) l(%d) ->", c1, r1, h1, dist);
+            if (max < dist) {
+                max = dist;
+            }
+            for (int i=0; i<4; ++i) {
+                int r2 = r1 + mat_row[i];
+                int c2 = c1 + mat_col[i];
+                if (isValid(r2, c2) == true) {
+                    int h2 = arr[c2][r2];
+                    if (h2 < h1) {
+                        visited[c2][r2] = true;
+                        dfs(h2, r2, c2, kcut, dist+1);
+                        visited[c2][r2] = false;
+                    } else if (kcut == false && ((h2-h1) < k)) {
+                        visited[c2][r2] = true;
+                        dfs(h1-1, r2, c2, true, dist+1);
+                        visited[c2][r2] = false;
+                    }
+                }
+            }
+            dbg_msg("backtrack!\n");
+        }
+        int answer() {
+            return max;
+        }
+
+        bool isValid(int row, int col) {
+            return row >= 0 && row < n && col >= 0 && col < n && visited[col][row] == false;
         }
     private:
-        int n, k;
+        int mat_row[4]{-1, 1, 0, 0};
+        int mat_col[4]{0, 0, -1, 1};
+        int n, k, max;
         int (*arr)[const_max_n];
-        xstack<coord> *st;
+        bool visited[const_max_n][const_max_n]{{false}};
+        xlist<coord> *list_peak;
 };
 
 void solve() {
@@ -86,7 +176,7 @@ void solve() {
     int arr[const_max_n][const_max_n]{0};
     int highest = -1;
     int max_trail = -1;
-    xstack<coord> st;
+    xlist<coord> list_peak;
 
     // input
     scanf("%d %d", &n, &k);
@@ -104,20 +194,26 @@ void solve() {
     for (int i=0; i<n; ++i) {
         for (int j=0; j<n; ++j) {
             if (arr[i][j] == highest) {
-                st.push_back(coord(i, j));
+                list_peak.push_back(coord(j, i));
             }
         }
     }
-    solver sol(n, k, arr, &st);
+    solver sol(n, k, arr, &list_peak);
     sol.print();
+    sol.solve();
+    dbg_msg("ans:%d\n", sol.answer());
+    printf("%d\n", sol.answer());
 }
 int main() {
+#ifdef DBG_STDIN
+    freopen("inputs/swe_pretest_1949_input.txt", "r", stdin);
+#endif
     int t;
-    //scanf("%d", &t);
-    for (int i = 0; i < 1; ++i) {
-        //printf("#%d ", i+1);
-        freopen("inputs/swe_pretest_1949_input.txt", "r", stdin);
+    scanf("%d", &t);
+    for (int i = 0; i < t; ++i) {
+        //dbg_msg("#%d ", i+1);
+        printf("#%d ", i+1);
+        solve();
     }
-    solve();
     return 0;
 }
